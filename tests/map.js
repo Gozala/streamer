91af6683c6cb6db3442d354dd57615f3ebc28419
Irakli Gozalishvili <rfobic@gmail.com>
2011-06-07 16:41:23 +0200

    Adding few more things to jslint.

--- tests/map.js
+++ tests/map.js
@@ -1,7 +1,7 @@
 /* vim:set ts=2 sw=2 sts=2 expandtab */
 /*jshint asi: true newcap: true undef: true es5: true node: true devel: true
          forin: true */
-/*global define: true */
+/*global define: true setTimeout: true */
 
 (typeof define === "undefined" ? function ($) { $(require, exports, module) } : define)(function (require, exports, module, undefined) {
 
