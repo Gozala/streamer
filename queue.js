816c946197e4c8da1eeb4331ab24e62850f326c2
Irakli Gozalishvili <rfobic@gmail.com>
2011-09-06 11:33:06 +0200

    Implementation of queue.

---
+++ queue.js
@@ -0,0 +1,63 @@
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true undef: true es5: true node: true devel: true
+         forin: false latedef: false */
+/*global define: true */
+
+!(typeof define !== "function" ? function($){ $(require, exports, module); } : define)(function(require, exports, module, undefined) {
+
+'use strict';
+
+var nil // Shortcut for undefined.
+var slice = Array.prototype.slice, push = Array.prototype.push
+
+// Utility function for storing a privates state
+function _(target) {
+  if (target.valueOf.length === 0)
+    _.initialize(target)
+  return target.valueOf(_)
+}
+_.initialize = function initialize(target) {
+  var _valueOf = target.valueOf;
+  var privates = {};
+  target.valueOf = function valueOf(privates) {
+    return privates === _ ? privates : _valueOf.call(target, privates)
+  }
+}
+
+function update(_stream) {
+  var buffer = _stream.buffer, next = _stream.next, stop = _stream.stop
+  if (buffer.length) next(buffer.shift())
+  return _stream.stopped ? stop && stop(_stream.reason) : update(_stream)
+}
+
+function queue() {
+  var _stream
+  function stream(next, stop) {
+    if (_stream.next) throw new Error("stream supports one reader only")
+    _stream.next = next
+    _stream.stop = stop
+    update(_stream)
+  }
+  _stream = _(stream)
+  _stream.buffer = slice.call(arguments)
+  _stream.stopped = false
+  return stream
+}
+exports.queue = queue
+
+function enqueue(queue) {
+  var _stream = _(queue)
+  push.apply(_stream.buffer, slice.call(arguments, 1))
+  update(_stream)
+}
+exports.enqueue = enqueue
+
+function stop(queue, reason) {
+  var _stream = _(queue)
+  _stream.stopped = true
+  _stream.reason = reason
+  update(_stream)
+}
+exports.stop = stop
+
+});
