e58529778d7f2a7dca6fe81c9c281e0dbc6020d4
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-18 21:40:53 +0400

    Update tests for zip.

--- tests/zip.js
+++ tests/zip.js
@@ -7,62 +7,57 @@
 
 'use strict';
 
-var streamer = require('../core.js'),
-    zip = streamer.zip, list = streamer.list, delay = streamer.delay,
-    append = streamer.append
-var test = require('./utils.js').test
 
-exports['test zip with empty'] = function(assert, done) {
-  var empty = list()
-  var numbers = list(1, 2, 3)
-  var zipped = zip(empty, numbers)
+var Stream = require('../core').Stream
 
-  test(assert, done, zipped, [])
+exports.Assert = require('./assert').Assert
+
+exports['test zip with empty'] = function(expect, complete) {
+  var actual = Stream.empty.zip(Stream.of(1, 2))
+
+  expect(actual).to.be.empty().then(complete)
 }
 
-exports['test zip 2 lists'] = function(assert, done) {
-  var numbers = list(1, 2, 3, 4)
-  var letters = list('a', 'b', 'c', 'd')
-  var zipped = zip(numbers, letters)
-  test(assert, done, zipped, [ [ 1, 'a' ], [ 2, 'b' ], [ 3, 'c' ], [ 4, 'd' ] ])
+exports['test zip 2 lists'] = function(expect, complete) {
+  var actual = Stream.of(1, 2, 3, 4).zip(Stream.of('a', 'b', 'c', 'd'))
+
+  expect(actual).to.be([ 1, 'a' ], [ 2, 'b' ], [ 3, 'c' ], [ 4, 'd' ]).
+  then(complete)
 }
 
-exports['test zip sync stream with async stream'] = function(assert, done) {
+/*
+exports['test zip sync stream with async stream'] = function(expect, complete) {
   var a = delay(list(5, 4, 3, 2, 1))
   var b = list('a', 'b', 'c', 'd', 'e')
   var c = list('~', '@', '!', '#')
 
   var zipped = zip(a, b, c)
 
-  test(assert, done, zipped, [
+  test(expect, complete, zipped, [
     [ 5, 'a', '~'  ],
     [ 4, 'b', '@' ],
     [ 3, 'c', '!' ],
     [ 2, 'd', '#' ]
   ])
 }
+*/
 
-exports['test zip with late error'] = function(assert, done) {
-  var error = Error('boom')
-  var stream = delay(append(list(3, 2, 1), function(next) { next(error) }))
-  var letters = list('a', 'b', 'c')
-  var zipped = zip(letters, stream)
+exports['test zip with late error'] = function(expect, complete) {
+  var boom = Error('boom')
+  var actual = Stream.of(3, 2, 1).append(Stream.error(boom)).delay().
+               zip(Stream.of('a', 'b', 'c'))
 
-  test(assert, done, zipped, [
-    [ 'a', 3 ],
-    [ 'b', 2 ],
-    [ 'c', 1 ]
-  ])
+  expect(actual).to.have.elements([ 3, 'a' ], [ 2, 'b' ], [ 1, 'c' ]).
+                 and.then(complete)
 }
 
-exports['test zip with early error'] = function(assert, done) {
-  var error = Error('Boom!!')
-  var stream = delay(append(list(3, 2, 1), function(next) { next(error) }))
-  var letters = list('a', 'b', 'c', 'd')
-  var actual = zip(stream, letters)
-  var expected = [ [ 3, 'a' ], [ 2, 'b' ], [ 1, 'c' ] ]
+exports['test zip with early error'] = function(expect, complete) {
+  var boom = Error('Boom!!')
+  var actual = Stream.of(1, 2, 3).append(Stream.error(boom)).delay().
+               zip(Stream.of('a', 'b', 'c', 'd'))
 
-  test(assert, done, actual, expected, error)
+  expect(actual).to.have.elements([ 1, 'a' ], [ 2, 'b' ], [ 3, 'c' ]).
+                 and.error(boom).then(complete)
 }
 
 if (module == require.main)
