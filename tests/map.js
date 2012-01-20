6ba372e37948ac1d6f3595b0ac78e97d902749c1
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-21 03:33:28 +0400

    Update map tests.

--- tests/map.js
+++ tests/map.js
@@ -7,33 +7,34 @@
 
 'use strict';
 
-var Stream = require('../core').Stream
+var streamer = require('../core'), Stream = streamer.Stream,
+    map = streamer.map, take = streamer.take, delay = streamer.delay,
+    append = streamer.append
 
 exports.Assert = require('./assert').Assert
 exports['test map empty'] = function(test, complete) {
-  var actual = Stream.empty.map(function(element) {
+  var actual = map(function(element) {
     test.fail('map fn was executed')
-  })
+  }, Stream.empty)
   test(actual).to.be.empty().then(complete)
 }
 
 exports['test number map'] = function(test, complete) {
   var numbers = Stream.of(1, 2, 3, 4)
-  var actual = numbers.map(function(number) { return number * 2 })
+  var actual = map(function(number) { return number * 2 }, numbers)
   test(actual).to.be(2, 4, 6, 8).then(complete)
 }
 
 exports['test map with async stream'] = function(test, complete) {
-  var stream = Stream.of(5, 4, 3, 2, 1).delay()
-  var mapped = stream.map(function(x) { return x + 1 })
-  test(mapped).to.be(6, 5, 4, 3, 2).then(complete)
+  var source = delay(Stream.of(5, 4, 3, 2, 1))
+  var actual = map(function(x) { return x + 1 }, source)
+  test(actual).to.be(6, 5, 4, 3, 2).then(complete)
 }
 
 exports['test map broken stream'] = function(test, complete) {
   var boom = Error('Boom!')
-  var stream = Stream.of(3, 2, 1).append(Stream.error(boom)).delay()
-  var actual = stream.map(function(x) { return x * x })
-  var expected = [ 9, 4, 1 ]
+  var source = append(Stream.of(3, 2, 1), Stream.error(boom))
+  var actual = map(function(x) { return x * x }, delay(source))
 
   test(actual).to.have.elements(9, 4, 1).and.an.error(boom).then(complete)
 }
