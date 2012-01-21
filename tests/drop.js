e68d2f1b242d11ee111d8721a03d352dae6ceefd
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-21 18:21:51 +0400

    Update `drop` tests.

--- tests/drop.js
+++ tests/drop.js
@@ -7,52 +7,58 @@
 
 'use strict';
 
-var Stream = require('../core').Stream
+var streamer = require('../core'), Stream = streamer.Stream,
+    append = streamer.append, delay = streamer.delay, drop = streamer.drop
 
 exports.Assert = require('./assert').Assert
 
-exports['test drop empty'] = function(test, complete) {
-  var actual = Stream.empty.drop(100)
-  test(actual).to.be.empty().then(complete)
+exports['test drop empty'] = function(expect, complete) {
+  var actual = drop(100, Stream.empty)
+  expect(actual).to.be.empty().then(complete)
 }
 
-exports['test drop on sync stream'] = function(test, complete) {
-  var actual = Stream.of(1, 2, 3, 4)
-  test(actual.drop(3)).to.be(4).then(complete)
+exports['test drop on sync stream'] = function(expect, complete) {
+  var actual = drop(3, Stream.of(1, 2, 3, 4))
+
+  expect(actual).to.be(4).then(complete)
 }
 
-exports['test drop falls back to 1'] = function(test, complete) {
-  var actual = Stream.of(1, 2, 3, 4)
-  test(actual.drop()).to.be(2, 3, 4).then(complete)
+exports['test drop falls back to 1'] = function(expect, complete) {
+  var actual = drop(Infinity, Stream.of(1, 2, 3, 4))
+
+  expect(actual).to.be.empty().then(complete)
 }
 
-exports['test drop can take 0'] = function(test, complete) {
-  var actual = Stream.of(1, 2, 3, 4)
-  test(actual.drop(0)).to.be(1, 2, 3, 4).then(complete)
+exports['test drop can take 0'] = function(expect, complete) {
+  var actual = drop(0, Stream.of(1, 2, 3, 4))
+
+  expect(actual).to.be(1, 2, 3, 4).then(complete)
 }
 
-exports['test drop more than have'] = function(test, complete) {
-  var actual = Stream.of(1, 2, 3, 4)
-  test(actual.drop(5)).to.be.empty().then(complete)
+exports['test drop more than have'] = function(expect, complete) {
+  var actual = drop(5, Stream.of(1, 2, 3, 4))
+
+  expect(actual).to.be.empty().then(complete)
 }
 
-exports['test drop of async stream'] = function(test, complete) {
-  var actual = Stream.of(5, 4, 3, 2, 1).delay()
-  test(actual.drop(2)).to.be(3, 2, 1).then(complete)
+exports['test drop of async stream'] = function(expect, complete) {
+  var actual = drop(2, delay(Stream.of(5, 4, 3, 2, 1)))
+
+  expect(actual).to.be(3, 2, 1).then(complete)
 }
 
-exports['test drop on stream with error'] = function(test, complete) {
+exports['test drop on stream with error'] = function(expect, complete) {
   var boom = Error('Boom!')
-  var actual = Stream.of(4, 3, 2, 1).append(Stream.error(boom)).delay()
+  var actual = drop(2, delay(append(Stream.of(4, 3, 2, 1), Stream.error(boom))))
 
-  test(actual.drop(2)).to.have.elements(2, 1).and.error(boom).then(complete)
+  expect(actual).to.have.elements(2, 1).and.error(boom).then(complete)
 }
 
-exports['test drop on stream with error in head'] = function(test, complete) {
+exports['test drop on stream with error in head'] = function(expect, complete) {
   var boom = Error('Boom!')
-  var actual = Stream.error(boom).append(Stream.of(4, 3, 2, 1)).delay()
+  var actual = drop(2, delay(append(Stream.error(boom), Stream.of(4, 3, 2, 1))))
 
-  test(actual.drop(2)).to.have.elements().and.error(boom).then(complete)
+  expect(actual).to.have.elements().and.error(boom).then(complete)
 }
 
 if (module == require.main)
