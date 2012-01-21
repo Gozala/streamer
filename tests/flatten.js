ef8a93dba286e2843305deff8740c784bac3d375
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-21 19:23:02 +0400

    Update `flatten` tests.

--- tests/flatten.js
+++ tests/flatten.js
@@ -7,47 +7,52 @@
 
 'use strict';
 
-var Stream = require('../core').Stream
+var streamer = require('../core'), Stream = streamer.Stream,
+    append = streamer.append, delay = streamer.delay, flatten = streamer.flatten
 
 exports.Assert = require('./assert').Assert
 
-exports['test flatten stream of empty streams'] = function(test, complete) {
-  var actual = Stream.of(Stream.empty, Stream.empty).flatten()
-  test(actual).to.be.empty().then(complete)
+exports['test flatten stream of empty streams'] = function(expect, complete) {
+  var actual = flatten(Stream.of(Stream.empty, Stream.empty))
+
+  expect(actual).to.be.empty().then(complete)
 }
 
-exports['test flatten empty & non-empty'] = function(test, complete) {
-  var actual = Stream.of(Stream.empty, Stream.of(1, 2), Stream.empty).flatten()
-  test(actual).to.be(1, 2).then(complete)
+exports['test flatten empty & non-empty'] = function(expect, complete) {
+  var actual = flatten(Stream.of(Stream.empty, Stream.of(1, 2), Stream.empty))
+
+  expect(actual).to.be(1, 2).then(complete)
 }
 
-exports['test flatten flattened'] = function(test, complete) {
-  var stream = Stream.of(Stream.of(1, 2), Stream.of('a', 'b')).flatten()
-  var actual = Stream.of(Stream.of('>'), stream, Stream.empty).flatten()
-  test(actual).to.be('>', 1, 2, 'a', 'b').then(complete)
+exports['test flatten flattened'] = function(expect, complete) {
+  var stream = flatten(Stream.of(Stream.of(1, 2), Stream.of('a', 'b')))
+  var actual = flatten(Stream.of(Stream.of('>'), stream, Stream.empty))
+
+  expect(actual).to.be('>', 1, 2, 'a', 'b').then(complete)
 }
 
-exports['test flatten sync & async streams'] = function(test, complete) {
-  var async = Stream.of(3, 2, 1).delay()
-  var actual = Stream.of(async, Stream.of('|'), async, Stream.of('a', 'b'),
-                         Stream.empty).flatten()
-  test(actual).to.be(3, 2, 1, '|', 3, 2, 1, 'a', 'b').then(complete)
+exports['test flatten sync & async streams'] = function(expect, complete) {
+  var async = delay(Stream.of(3, 2, 1))
+  var actual = flatten(Stream.of(async, Stream.of('|'), async,
+                                 Stream.of('a', 'b'), Stream.empty))
+
+  expect(actual).to.be(3, 2, 1, '|', 3, 2, 1, 'a', 'b').then(complete)
 }
 
-exports['test flatten with broken stream'] = function(test, complete) {
+exports['test flatten with broken stream'] = function(expect, complete) {
   var boom = Error('Boom!')
-  var async = Stream.of(3, 2, 1).append(Stream.error(boom)).delay()
-  var actual = Stream.of(Stream.of('>'), async, Stream.of(1, 2)).flatten()
+  var async = delay(append(Stream.of(3, 2, 1), Stream.error(boom)))
+  var actual = flatten(Stream.of(Stream.of('>'), async, Stream.of(1, 2)))
 
-  test(actual).to.have.elements('>', 3, 2, 1).and.error(boom).then(complete)
+  expect(actual).to.have.elements('>', 3, 2, 1).and.error(boom).then(complete)
 }
 
-exports['test flatten async stream of streams'] = function(test, complete) {
-  var async = Stream.of(3, 2, 1).delay()
-  var actual = Stream.of(Stream.empty, Stream.of(1, 2), async,
-                         Stream.of('a', 'b'), async).flatten()
+exports['test flatten async stream of streams'] = function(expect, complete) {
+  var async = delay(Stream.of(3, 2, 1))
+  var actual = flatten(Stream.of(Stream.empty, Stream.of(1, 2), async,
+                                 Stream.of('a', 'b'), async))
 
-  test(actual).to.be(1, 2, 3, 2, 1, 'a', 'b', 3, 2, 1).then(complete)
+  expect(actual).to.be(1, 2, 3, 2, 1, 'a', 'b', 3, 2, 1).then(complete)
 }
 
 if (module == require.main)
