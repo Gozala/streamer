6804a02724c520cc940c78e16e01dbbd3b5dbd73
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-21 20:33:06 +0400

    Update mix tests.

--- tests/mix.js
+++ tests/mix.js
@@ -7,56 +7,57 @@
 
 'use strict';
 
-var Stream = require('../core').Stream
+var streamer = require('../core'), Stream = streamer.Stream,
+    append = streamer.append, delay = streamer.delay, mix = streamer.mix
 
 exports.Assert = require('./assert').Assert
 
 exports['test mix empty streams'] = function(expect, complete) {
-  var actual = Stream.empty.mix(Stream.empty)
-  expect(actual).to.be.empty().and.then(complete)
+  var actual = mix(Stream.empty, Stream.empty)
+
+  expect(actual).to.be.empty().then(complete)
 }
 
 exports['test mix with empty'] = function(expect, complete) {
-  var actual = Stream.of(1, 2).mix(Stream.of())
+  var actual = mix(Stream.of(1, 2), Stream.of())
+
   expect(actual).to.be(1, 2).then(complete)
 }
 
 exports['test mix to empty'] = function(expect, complete) {
-  var actual = Stream.empty.mix(Stream.of(3, 4))
+  var actual = mix(Stream.empty, Stream.of(3, 4))
+
   expect(actual).to.be(3, 4).then(complete)
 }
 
 exports['test mix many streams'] = function(expect, complete) {
-  var actual = Stream.of(1, 2).mix(Stream.empty).
-                               mix(Stream.of('a', 'b')).
-                               mix(Stream.empty)
+  var actual = mix.all(Stream.of(1, 2), Stream.empty, Stream.of('a', 'b'),
+                       Stream.empty)
 
   expect(actual).to.be(1, 'a', 2, 'b').then(complete)
 }
 
 exports['test mix sync & async streams'] = function(expect, complete) {
-  var async = Stream.of(3, 2, 1).delay()
-  var actual = async.mix(Stream.empty).
-                     mix(async).
-                     mix(Stream.of('a', 'b'))
+  var async = delay(Stream.of(3, 2, 1))
+  var actual = mix.all(async, Stream.empty, async, Stream.of('a', 'b'))
+
   expect(actual).to.be('a', 'b', 3, 3, 2, 2, 1, 1).then(complete)
 }
 
 exports['test mix & remix'] = function(expect, complete) {
-  var async = Stream.of(3, 2, 1).delay()
-  var mixed = async.mix(Stream.of('a', 'b'))
-  var actual = mixed.mix(Stream.of('||').mix(mixed))
+  var async = delay(Stream.of(2, 1))
+  var mixed = mix(async, Stream.of('a', 'b'))
+  var actual = mix(mix(mixed, Stream.of('||')), mixed)
 
-  expect(actual).to.be('a', '||', 'b', 'a', 'b', 3, 3, 2, 2, 1, 1).then(complete)
+  expect(actual).to.be('a', 'a', '||', 'b', 'b', 2, 2, 1, 1).then(complete)
 }
 
-exports['test mix broken stream'] = function(expect, complete) {
+exports['test errors propagate to mixed streams'] = function(expect, complete) {
   var boom = Error('Boom!!')
-  var broken = Stream.error(boom)
-  var async = Stream.of(3, 2, 1).mix(broken).delay()
-  var actual = Stream.of('>').mix(async).mix(Stream.of('a', 'b')).mix(async)
+  var async = delay(mix(Stream.of(3, 2, 1), Stream.error(boom)))
+  var actual = mix.all(Stream.of('>'), async, Stream.of('a', 'b'), async)
 
-  expect(actual).to.have.elements('>', 'a', 'b', 3).and.error(boom).then(complete)
+  expect(actual).to.have('>', 'a', 'b', 3).and.error(boom).then(complete)
 }
 
 if (module == require.main)
