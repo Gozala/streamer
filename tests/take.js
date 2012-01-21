2d5f0fed22bca97a486aa08337f91ab0ecb70499
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-21 18:56:48 +0400

    Add `take.while` tests.

--- tests/take.js
+++ tests/take.js
@@ -13,43 +13,112 @@ var streamer = require('../core'), Stream = streamer.Stream,
 
 exports.Assert = require('./assert').Assert
 
-exports['test take empty'] = function(test, complete) {
+exports['test take empty'] = function(expect, complete) {
   var actual = take(100, Stream.empty)
-  test(actual).to.be.empty().then(complete)
+
+  expect(actual).to.be.empty().then(complete)
 }
 
-exports['test take more than have'] = function(test, complete) {
+exports['test take.while on empty'] = function(expect, complete) {
+  var called = 0
+  var actual = take['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, Stream.empty)
+
+  expect(actual).to.be.empty().then(function(assert) {
+    assert.equal(called, 0, 'nothing to take')
+    complete()
+  })
+}
+
+exports['test take more than have'] = function(expect, complete) {
   var actual = Stream.of(1, 2, 3)
-  test(take(5, actual)).to.be(1, 2, 3).then(complete)
+
+  expect(take(5, actual)).to.be(1, 2, 3).then(complete)
 }
 
-exports['test take falls back to all'] = function(test, complete) {
+exports['test take falls back to all'] = function(expect, complete) {
   var actual = Stream.of(1, 2, 3)
-  test(take(Infinity, actual)).to.be(1, 2, 3).then(complete)
+
+  expect(take(Infinity, actual)).to.be(1, 2, 3).then(complete)
 }
 
-exports['test take may be given 0'] = function(test, complete) {
+exports['test take may be given 0'] = function(expect, complete) {
   var actual = Stream.of(1, 2, 3)
-  test(take(0, actual)).to.be.empty().then(complete)
+
+  expect(take(0, actual)).to.be.empty().then(complete)
 }
 
-exports['test take on async stream'] = function(test, complete) {
+exports['test take.while'] = function(expect, complete) {
+  var called = 0
+  var actual = take['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+
+  expect(actual).to.be(1, 2, 3, 4, 5, 6, 7, 8, 9).then(function(assert) {
+    assert.equal(called, 10, 'called until returned false')
+    complete()
+  })
+}
+
+exports['test take.while end'] = function(expect, complete) {
+  var called = 0
+  var actual = take['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, Stream.of(1, 2, 3, 4, 5, 6, 7))
+
+  expect(actual).to.be(1, 2, 3, 4, 5, 6, 7).then(function(assert) {
+    assert.equal(called, 7, 'called until returned false')
+    complete()
+  })
+}
+
+exports['test take on async stream'] = function(expect, complete) {
   var actual = delay(Stream.of(5, 4, 3, 2, 1))
-  test(take(3, actual)).to.be(5, 4, 3).then(complete)
+  expect(take(3, actual)).to.be(5, 4, 3).then(complete)
 }
 
-exports['test take before error'] = function(test, complete) {
+exports['test take.while on async stream'] = function(expect, complete) {
+  var called = 0
+  var actual = take['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, delay(Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)))
+
+  expect(actual).to.be(1, 2, 3, 4, 5, 6, 7, 8, 9).then(function(assert) {
+    assert.equal(called, 10, 'called until returned false')
+    complete()
+  })
+}
+
+exports['test take before error'] = function(expect, complete) {
   var boom = Error('Boom!')
   var actual = delay(append(Stream.of(3, 2, 1), Stream.error(boom)))
 
-  test(take(3, actual)).to.be(3, 2, 1).then(complete)
+  expect(take(3, actual)).to.be(3, 2, 1).then(complete)
 }
 
-exports['test error on take'] = function(test, complete) {
+exports['test error propagation'] = function(expect, complete) {
   var boom = Error('Boom!')
   var actual = delay(append(Stream.of(3, 2, 1), Stream.error(boom)))
 
-  test(take(5, actual)).to.have.elements(3, 2, 1).and.error(boom).then(complete)
+  expect(take(5, actual)).to.have.elements(3, 2, 1).and.error(boom).then(complete)
+}
+
+exports['test error propagation in take.while'] = function(expect, complete) {
+  var called = 0, boom = Error('boom!')
+  var actual = take['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, append.all(Stream.of(1, 2, 3, 4, 5), Stream.error(boom), Stream.of(6, 7)))
+
+  expect(actual).to.have(1, 2, 3, 4, 5).and.error(boom).then(function(assert) {
+    assert.equal(called, 5, 'called until error')
+    complete()
+  })
 }
 
 if (module == require.main)
