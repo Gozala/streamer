ec1a3c792edbea53b09d32f8279a7fd42b5306e2
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-22 01:53:36 +0400

    Refactor zipmap tests to `map.all` tests.

--- tests/map.js
+++ tests/map.js
@@ -12,31 +12,79 @@ var streamer = require('../core'), Stream = streamer.Stream,
     append = streamer.append
 
 exports.Assert = require('./assert').Assert
-exports['test map empty'] = function(test, complete) {
+exports['test map empty'] = function(expect, complete) {
   var actual = map(function(element) {
     test.fail('map fn was executed')
   }, Stream.empty)
-  test(actual).to.be.empty().then(complete)
+  expect(actual).to.be.empty().then(complete)
 }
 
-exports['test number map'] = function(test, complete) {
+exports['test map.all with empty'] = function(expect, complete) {
+  var actual = map(function(a, b) {
+    return a + b
+  }, Stream.empty, Stream.of(1, 2, 3, 4))
+
+  expect(actual).to.be.empty().then(complete)
+}
+
+exports['test number map'] = function(expect, complete) {
   var numbers = Stream.of(1, 2, 3, 4)
   var actual = map(function(number) { return number * 2 }, numbers)
-  test(actual).to.be(2, 4, 6, 8).then(complete)
+  expect(actual).to.be(2, 4, 6, 8).then(complete)
 }
 
-exports['test map with async stream'] = function(test, complete) {
+exports['test map.all same length streams'] = function(expect, complete) {
+  var actual = map.all(function(a, b) {
+    return a + b
+  }, Stream.of(1, 2, 3, 4), Stream.from('abcd'))
+
+  expect(actual).to.be('1a', '2b', '3c', '4d').then(complete)
+}
+
+exports['test map with async stream'] = function(expect, complete) {
   var source = delay(Stream.of(5, 4, 3, 2, 1))
   var actual = map(function(x) { return x + 1 }, source)
-  test(actual).to.be(6, 5, 4, 3, 2).then(complete)
+  expect(actual).to.be(6, 5, 4, 3, 2).then(complete)
 }
 
-exports['test map broken stream'] = function(test, complete) {
+exports['test map.all sync & async stream'] = function(expect, complete) {
+  var actual = map.all(Array, delay(Stream.of(5, 4, 3, 2, 1)),
+                       Stream.from('abcde'), Stream.from('~@!#'))
+
+  expect(actual).to.be(
+    [ 5, 'a', '~' ],
+    [ 4, 'b', '@' ],
+    [ 3, 'c', '!' ],
+    [ 2, 'd', '#' ]
+  ).then(complete)
+}
+
+exports['test map broken stream'] = function(expect, complete) {
   var boom = Error('Boom!')
   var source = append(Stream.of(3, 2, 1), Stream.error(boom))
   var actual = map(function(x) { return x * x }, delay(source))
 
-  test(actual).to.have.items(9, 4, 1).and.an.error(boom).then(complete)
+  expect(actual).to.have.items(9, 4, 1).and.an.error(boom).then(complete)
+}
+
+exports['test map.all with late error'] = function(expect, complete) {
+  var boom = Error('boom')
+  var actual = map.all(function(a, b) {
+    return a + b
+  }, Stream.from('abc'), delay(append(Stream.of(3, 2, 1), Stream.error(boom))))
+
+  expect(actual).to.be('a3', 'b2', 'c1').then(complete)
+}
+
+exports['test map.all with early error'] = function(expect, complete) {
+  var boom = Error('Boom!!')
+  var actual = map.all(Array,
+                       delay(append(Stream.of(3, 2, 1), Stream.error(boom))),
+                       Stream.from('abcd'))
+
+
+  expect(actual).to.have.items([ 3, 'a' ], [ 2, 'b' ], [ 1, 'c' ]).
+                 and.error(boom).then(complete)
 }
 
 if (module == require.main)
