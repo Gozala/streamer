913319353c3ca13d5dd634567be55b61a3832919
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-21 19:10:07 +0400

    Implement tests for `drop.while` function.

--- tests/drop.js
+++ tests/drop.js
@@ -17,6 +17,19 @@ exports['test drop empty'] = function(expect, complete) {
   expect(actual).to.be.empty().then(complete)
 }
 
+exports['test take.while on empty'] = function(expect, complete) {
+  var called = 0
+  var actual = drop['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, Stream.empty)
+
+  expect(actual).to.be.empty().then(function(assert) {
+    assert.equal(called, 0, 'nothing to take')
+    complete()
+  })
+}
+
 exports['test drop on sync stream'] = function(expect, complete) {
   var actual = drop(3, Stream.of(1, 2, 3, 4))
 
@@ -35,6 +48,32 @@ exports['test drop can take 0'] = function(expect, complete) {
   expect(actual).to.be(1, 2, 3, 4).then(complete)
 }
 
+exports['test drop.while'] = function(expect, complete) {
+  var called = 0
+  var actual = drop['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+
+  expect(actual).to.be(10, 11, 12).then(function(assert) {
+    assert.equal(called, 10, 'called until returned false')
+    complete()
+  })
+}
+
+exports['test drop.while end'] = function(expect, complete) {
+  var called = 0
+  var actual = drop['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, Stream.of(1, 2, 3, 4, 5, 6, 7))
+
+  expect(actual).to.be.empty().then(function(assert) {
+    assert.equal(called, 7, 'called until returned false')
+    complete()
+  })
+}
+
 exports['test drop more than have'] = function(expect, complete) {
   var actual = drop(5, Stream.of(1, 2, 3, 4))
 
@@ -45,6 +84,19 @@ exports['test drop of async stream'] = function(expect, complete) {
   var actual = drop(2, delay(Stream.of(5, 4, 3, 2, 1)))
 
   expect(actual).to.be(3, 2, 1).then(complete)
+}
+
+exports['test drop.while on async stream'] = function(expect, complete) {
+  var called = 0
+  var actual = drop['while'](function(n) {
+    called = called + 1
+    return n <= 9
+  }, delay(Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)))
+
+  expect(actual).to.be(10, 11, 12).then(function(assert) {
+    assert.equal(called, 10, 'called until returned false')
+    complete()
+  })
 }
 
 exports['test drop on stream with error'] = function(expect, complete) {
@@ -59,6 +111,19 @@ exports['test drop on stream with error in head'] = function(expect, complete) {
   var actual = drop(2, delay(append(Stream.error(boom), Stream.of(4, 3, 2, 1))))
 
   expect(actual).to.have.elements().and.error(boom).then(complete)
+}
+
+exports['test error propagation in drop.while'] = function(expect, complete) {
+  var called = 0, boom = Error('boom!')
+  var actual = drop['while'](function(n) {
+    called = called + 1
+    return n <= 3
+  }, append.all(Stream.of(1, 2, 3, 4, 5), Stream.error(boom), Stream.of(6, 7)))
+
+  expect(actual).to.have(4, 5).and.error(boom).then(function(assert) {
+    assert.equal(called, 4, 'called until error')
+    complete()
+  })
 }
 
 if (module == require.main)
