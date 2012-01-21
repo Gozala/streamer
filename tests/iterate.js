64ee804c659fbd3f927987608f227e6fd4113ceb
Irakli Gozalishvili <rfobic@gmail.com>
2012-01-21 21:16:51 +0400

    Update tests for `iterate`.

--- tests/iterate.js
+++ tests/iterate.js
@@ -7,12 +7,15 @@
 
 'use strict';
 
-var Stream = require('../core').Stream
+var streamer = require('../core'), Stream = streamer.Stream,
+    take = streamer.take, iterate = streamer.iterate
 
 exports.Assert = require('./assert').Assert
 exports['test basic iterate'] = function(expect, complete) {
-  var numbers = Stream.iterate(function incriment(x) { return x + 1 }, 0)
-  expect(numbers.take(5)).to.be(0, 1, 2, 3, 4).and.then(complete)
+  var actual = iterate(function incriment(x) { return x + 1 }, 0)
+
+  expect(take(5, actual)).to.be(0, 1, 2, 3, 4)
+  expect(take(10, actual)).to.be(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).then(complete)
 }
 
 if (module == require.main)
