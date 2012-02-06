3193c94f7b7e81ea7c63a2efcedf6c299cf73de2
Irakli Gozalishvili <rfobic@gmail.com>
2012-02-05 23:44:25 -0800

    Update tests for reduce.

--- tests/reduce.js
+++ tests/reduce.js
@@ -1,5 +1,5 @@
 /* vim:set ts=2 sw=2 sts=2 expandtab */
-/*jshint asi: true newcap: true undef: true es5: true node: true devel: true
+/*jshint asi: true undef: true es5: true node: true devel: true
          forin: true */
 /*global define: true setTimeout: true */
 
@@ -7,57 +7,43 @@
 
 'use strict';
 
-var streamer = require('../core.js'),
-    reduce = streamer.reduce, list = streamer.list
-var test = require('./utils.js').test
+var streamer = require('../core'), Stream = streamer.Stream,
+    map = streamer.map, take = streamer.take, delay = streamer.delay,
+    append = streamer.append, reduce = streamer.reduce
 
-exports['test reduce on empty'] = function(assert, done) {
-  var empty = list()
-  var reduced = reduce(function onEach(element) {
-    assert.fail('reducer was called on empty list')
-  }, empty, null)
-  test(assert, done, reduced, [ null ])
+exports.Assert = require('./assert').Assert
+exports['test reduce on empty'] = function(expect, complete) {
+  var called = 0
+  var actual = reduce(function(item) {
+    expect.test.fail('reducer was called on empty stream')
+  }, Stream.empty, null)
+
+  expect(actual).to.be(null).then(complete)
 }
 
-exports['test reduce to sum'] = function(assert, done) {
-  var numbers = list(1, 2, 3, 4)
-  var sum = reduce(function onElement(previous, current) {
-    return (previous || 0) + current
-  }, numbers)
-  test(assert, done, sum, [ 10 ])
+exports['test reduce to sum'] = function(expect, complete) {
+  var actual = reduce(function(x, y) {
+    return x + y
+  }, Stream.of(1, 2, 3, 4), 0)
+
+  expect(actual).to.be(10).then(complete)
 }
 
-exports['test reduce async stream'] = function(assert, done) {
-  function stream(next, stop) {
-    var x = 5
-    setTimeout(function onTimeout() {
-      if (!x) return stop()
-      next(x--)
-      setTimeout(onTimeout, 0)
-    }, 0)
-  }
-  var sum = reduce(function(previous, current) {
-    return previous + current
-  }, stream, 3)
-  test(assert, done, sum, [ 18 ])
+exports['test reduce async stream'] = function(expect, complete) {
+  var actual = reduce(function(x, y) {
+    return x + y
+  }, delay(Stream.of(5, 4, 3, 2, 1)), 3)
+
+  expect(actual).to.be(18).then(complete)
 }
 
-exports['test reduce broken stream'] = function(assert, done) {
-  function stream(next, stop) {
-    var x = 3
-    setTimeout(function onTimeout() {
-      if (!x) return stop(new Error('Boom!'))
-      next(x--)
-      setTimeout(onTimeout, 0)
-    }, 0)
-  }
-  var sum = reduce(function(x, y) { return x + y }, stream)
-  sum(function next(x) {
-    assert.fail('should not yield value if stream failed')
-  }, function stop(error) {
-    assert.equal(error.message, 'Boom!', 'error propagated to reduced stream')
-    done()
-  })
+exports['test reduce broken stream'] = function(expect, complete) {
+  var boom = Error('Boom!')
+  var actual = reduce(function(x, y) {
+    return x + y
+  }, append(Stream.of(3, 2, 1), Stream.error(boom)), 0)
+
+  expect(actual).to.have.an.error(boom).then(complete)
 }
 
 if (module == require.main)
